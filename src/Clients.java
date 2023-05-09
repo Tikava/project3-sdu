d1de971922ecfffd53c53f9b8eac8d6cbcbe29cf
Tikava <wavoleniu@gmail.com>
2023-05-09 21:39:59 +0600

    Second part is done

--- src/Clients.java
+++ src/Clients.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Clients {
+import java.net.*;
+import java.io.*;
+
+public class Clients {
+    public static void main(String[] args) {
+
+        String hostName = "localhost";
+        int portNumber = 1024;
+
+        try (
+                Socket socket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            System.out.println("Request sent successfully");
+            String userInput;
+            String response;
+            while (true) {
+                if ((response = in.readLine()) != null) {
+                    System.out.println("Server: " + response);
+                }
+                System.out.print("Client: ");
+                if ((userInput = stdIn.readLine()) != null) {
+                    out.println(userInput);
+                }
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 }
--- src/MTServer.java
+++ src/MTServer.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class MTServer {
+import java.net.*;
+import java.io.*;
+
+public class MTServer {
+    public static void main(String[] args) {
+
+        int portNumber = 1024;
+        System.out.println("Waiting for client request");
+        try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                Socket clientSocket = serverSocket.accept();
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter outToClient = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            System.out.println("New client is pop up!");
+
+            String inputLine;
+            String outputLine;
+            while (true) {
+                System.out.print("Server: ");
+                if ((outputLine = stdIn.readLine()) != null) {
+                    outToClient.println(outputLine);
+                }
+
+                System.out.print("Clients: ");
+                if ((inputLine = inFromClient.readLine()) != null) {
+                    System.out.println(inputLine);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
